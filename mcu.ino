1db59997b1b9a519705387cf239060c27f9d3fe2
alvaro2302 <alvaro230297@hotmail.com>
2020-09-21 20:32:22 -0400

    receive data of MCU in the server

--- mcu.ino
+++ mcu.ino
@@ -1,11 +1,12 @@
 #include <WiFi.h>
 
-const char* ssid = "pon ssid de la red ";
-const char* password = "contraseña del wifi";
+const char* ssid = "COMTECO-95158360";
+const char* password = "70415428";
 
 const uint16_t port = 8090;
-const char * host = "192.168.1.83";
-
+const char * host = "192.168.1.72";
+String mensaje = " ";
+int size;
 
 void setup()
 {
@@ -22,11 +23,11 @@ void setup()
   Serial.println(WiFi.localIP());
  
 }
-
+  
 void loop()
 {
     WiFiClient client;
- 
+
     if (!client.connect(host, port)) {
  
         Serial.println("Connection to host failed");
@@ -36,9 +37,7 @@ void loop()
     }
  
     Serial.println("Connected to server successful!");
- 
-    client.print("Hello from ESP32!");
- 
+    client.print("Hello from ESP32! jojojoj");
     Serial.println("Disconnecting...");
     client.stop();
  
--- server.py
+++ server.py
@@ -1,9 +1,7 @@
 import socket
 
-HOST = '127.0.0.1'
-PORT = 65432
-
-
+HOST = '0.0.0.0'
+PORT = 8090
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST,PORT))
     s.listen()
