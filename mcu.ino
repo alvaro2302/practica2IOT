983db68cc6dc8128c8a8f78cd763130b1a1445cd
alvaro2302 <alvaro230297@hotmail.com>
2020-09-28 19:12:05 -0400

    resolving problem in read

--- mcu.ino
+++ mcu.ino
@@ -26,7 +26,7 @@ void setup()
   
 void loop()
 {
-    delay(1000)
+    delay(5000)
     WiFiClient client;
 
     if (!client.connect(host, port)) {
@@ -36,18 +36,28 @@ void loop()
         delay(1000);
         return;
     }
-    if(client.available() == 0){
+    unsigned long timeout = millis();
+    while (client.available() == 0) 
+    {
+      if (millis() - timeout > 2000) {
+        Serial.println("Client timeout!");
+        Serial.println("Connected to server successful!");
+        client.print("Hello from ESP32! jojojoj");
+        client.stop();
+        return;
+      }
+    }
+
+
+    if(client.available() > 0){
       String line = client.readStringUntil('\n');
+      Serial.print("recibido")
       Serial.print(line);
 
     }
  
-    Serial.println("Connected to server successful!");
-
     
-    client.print("Hello from ESP32! jojojoj");
     Serial.println("Disconnecting...");
     client.stop();
  
-    delay(10000);
 }
