afcd8c162bfd79041a7b3908cf6adfb991431c0d
alvaro2302 <alvaro230297@hotmail.com>
2020-09-28 18:05:04 -0400

    add send and recieve in client

--- server.py
+++ server.py
@@ -9,9 +9,19 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     with conn:
         print('Connected by ', addr)
         while True : 
+           
             data  = conn.recv(1024)
             print("esto es el data")
             print("data", data)
-            if not data:
-                break
+            # if not data:
+            #     break
+            
+            print("ingrese la opcion ")
+            print("1 prender led azul")
+            print("2 prender led rojo")
+            respuesta = input()
+            if(respuesta == "1" or respuesta == "2"):
+                conn.send(respuesta.encode())
+            
             conn.sendall(data)
+            
