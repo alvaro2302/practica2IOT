a8a1ba5d130a25e30ffc2c17e9246f841d8239b2
alvaro2302 <alvaro230297@hotmail.com>
2020-10-05 16:20:21 -0400

    automatic led on or off

--- server.py
+++ server.py
@@ -3,18 +3,21 @@ import threading
 
 def on_new_client(clientsocket,addr):
     while True : 
-        print("ingrese la opcion ")
-        print("1 prender led azul")
-        print("2 prender led rojo")
-        respuesta = input()
-        if(respuesta != " "):
-            conn.send(respuesta.encode())
-        if(str(respuesta) =="0"):
-            print("entro aqui")
-            break
+        # print("ingrese la opcion ")
+        # print("1 prender led azul")
+        # print("2 prender led rojo")
+        # respuesta = input()
+        
         data  = conn.recv(1024)
-        print("esto es el data")
+        print("esto es la longitud en cm")
         print("data", data)
+        
+        if(int(data) > 5 ):
+            respuesta = "ON"
+            conn.send(respuesta.encode())
+        if(int(data) > 10 ):
+            respuesta = "OFF"
+            conn.send(respuesta.encode())
         # if not data:
         #     break
             
